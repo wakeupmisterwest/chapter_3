18c5d88ac7257d958460823651cf1e0eb9205d91
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-04 11:47:47 -0400

    Add files via upload

---
+++ Starks_TIY_3_3.py
@@ -0,0 +1,3 @@
+cars = ["toyota", "ford", "chevrolet", "honda", "nisan"]
+print(f"I would like to own a car made by {cars[0].title()}.")
+print(f"I would like to own a truck made by {cars[1].title()}.")
