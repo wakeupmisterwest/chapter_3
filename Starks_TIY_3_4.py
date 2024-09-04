b3e51c01e25ce61699346668508494f4cd6b6681
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-04 12:49:45 -0400

    Add files via upload

---
+++ Starks_TIY_3_4.py
@@ -0,0 +1,7 @@
+invites = ["bob", "rob", "phil", "bill"]
+message = "You've been invited to my dinner,"
+
+print(f"{message} {invites[0].title()}")
+print(f"{message} {invites[1].title()}")
+print(f"{message} {invites[2].title()}")
+print(f"{message} {invites[3].title()}")
