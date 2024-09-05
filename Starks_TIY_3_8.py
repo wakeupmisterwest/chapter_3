064fcc228e18dce9552acb86e7022c4f2104d72e
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-05 12:06:01 -0400

    Add files via upload

---
+++ Starks_TIY_3_8.py
@@ -0,0 +1,15 @@
+places = ["Japan", "Hawaii", "Mexico", "France", "New zealand"]
+print(places)
+print(sorted(places))
+print(sorted(places, reverse=True))
+print(places)
+
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
